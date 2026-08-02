f3b762a4a4d5f43cac079fe9b58777fce3785e18
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2026-08-02 18:48:19 +0530

    Add next-sitemap configuration and update package.json for sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.snpskizhavoor.org",
+
+  generateRobotsTxt: false,
+
+  sitemapSize: 7000,
+
+  changefreq: "weekly",
+
+  priority: 0.7,
+
+  trailingSlash: false,
+};
